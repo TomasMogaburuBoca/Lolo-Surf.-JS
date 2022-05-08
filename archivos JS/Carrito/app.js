e3fcdf0b2825d371bacd710601dc1abf5ac354f3
TomasMogaburuBoca <tomasmogaburu@gmail.com>
2022-05-08 17:29:21 -0300

    Se agrega JS al proyecyo Lolo Surf

---
+++ archivos JS/Carrito/app.js
@@ -0,0 +1,181 @@
+let carritoDeCompras = [];
+
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+const botonTerminar = document.getElementById('terminar')
+
+const contadorCarrito = document.getElementById('contadorCarrito')
+const precioTotal = document.getElementById('precioTotal')
+
+const seleccionTipo = document.getElementById('seleccionTipo')
+const buscador = document.getElementById('search')
+
+//Filtro por tipo de producto
+seleccionTipo.addEventListener('change', () => {
+    if (seleccionTipo.value == "todos"){
+        mostrarProducto (stockProductos)
+    } else {
+        mostrarProducto( 
+            stockProductos.filter((elemento) => elemento.tipo == seleccionTipo.value)
+        )
+    }
+});
+buscador.addEventListener('keypress', () =>{
+    if (buscador.value == "todos"){
+        mostrarProducto(stockProductos)
+    } else {
+        mostrarProducto(
+            stockProductos.filter((elemento) => elemento.tipo == buscador.value)
+        )
+    }
+})
+contadorCarrito.addEventListener('click', () =>{
+    mostrarProducto(stockProductos);
+    console.log(contadorCarrito);
+})
+
+//Muestro los productos
+mostrarProducto(stockProductos);
+
+//Logica e-commerce
+function mostrarProducto(array){
+    contenedorProductos.innerHTML = '';
+
+    array.forEach((item) =>{
+        let div = document.createElement("div")
+        div.classList.add("producto")
+        div.innerHTML += `
+            <div class= "card">
+                <div class = "card-img">
+                    <img src=${item.img}>
+                    <span class= "card-title">${item.name}</span>
+                    <a  id="agregar${item.id}" class="btnAgregar">Agregar</i></a>
+                </div>
+                <div class="card-content">
+                    
+                    <p>Tipo: ${item.tipo}</p>
+                    <p>$${item.price}</p>
+                </div>
+            </div>
+        `;
+        contenedorProductos.appendChild(div);
+        
+        let btnAgregar = document.getElementById(`agregar${item.id}`)
+
+        btnAgregar.addEventListener('click', () =>{
+            agregarAlCarrito(item.id)
+            let productoAgregar = stockProductos.find(
+                (elemento) => elemento.id == item.id
+            )
+            localStorage.setItem('producto', JSON.stringify(productoAgregar))
+            
+            Toastify({
+                text: "Agregado al carrito",
+                className: "info",
+                position: 'center',
+                style: {
+                    background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+            }).showToast();
+        })
+    })
+}
+
+
+//Agregamos producto cliqueando al carrito
+function agregarAlCarrito(id){
+    let yaEsta = carritoDeCompras.find((item) => item.id == id)
+    console.log(yaEsta);
+    if (yaEsta){
+        yaEsta.cantidad = yaEsta.cantidad + 1;
+        document.getElementById(`und${yaEsta.id}`).innerHTML = `<p id=und${yaEsta.id}>Und: ${yaEsta.cantidad}</p>`;
+        actualizarCarrito();
+        setTimeout(() => {
+            Toastify({
+                text: "Este producto ya se encuentra en el carrito",
+                className: "info",
+                style: {
+                    background: "linear-gradient(to right, #fffff,#48c1e9, #add8e6)",
+                }
+            }).showToast();
+        }, 500);
+    } else {
+        let productoAgregar = stockProductos.find((elemento) => elemento.id == id);
+        productoAgregar.cantidad = 1;
+        carritoDeCompras.push(productoAgregar);
+        actualizarCarrito();
+        mostrarCarrito(productoAgregar);
+    }
+}
+
+//Muestro productos agregads al carrito
+function mostrarCarrito(productoAgregar){
+    let div = document.createElement("div");
+    div.className = "productoEnCarrito";
+    div.innerHTML = `
+        <p>${productoAgregar.name}</p>
+        <p>Precio: $${productoAgregar.price}</p>
+        <p id= "und${productoAgregar.id}">Und: ${productoAgregar.cantidad}</p>
+        <button id="eliminar${productoAgregar.id}" class="boton-eliminar"><i class="fas fa-trash-alt"></i>X</button>
+        `;
+    contenedorModal.appendChild(div);
+
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`);
+    btnEliminar.classList.add("btnEliminar");
+
+    btnEliminar.addEventListener('click', () => {
+        if (productoAgregar.cantidad == 1){
+            btnEliminar.parentElement.remove();
+            carritoDeCompras = carritoDeCompras.filter(
+                (item) => item.id != productoAgregar.id
+            );
+
+            Toastify({
+                text: "Este producto fue removido del carrito",
+                className: "info",
+                position: 'center',
+                style: {
+                    background: "linear-gradient(to right, #e91414, #ff0000)",
+                }
+            }).showToast();
+
+            actualizarCarrito();
+            localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
+        } else {
+            productoAgregar.cantidad = productoAgregar.cantidad - 1;
+            document.getElementById(
+                `und${productoAgregar.id}`
+            ).innerHTML = `<p id=und${productoAgregar.id}>Und:${productoAgregar.cantidad}</p>`;
+            actualizarCarrito();
+            localStorage.setItem('carrito', JSON.stringify(carritoDeCompras));
+        }
+    });
+}
+
+
+//Actualizacion de "CANTIDAD" y "PRECIO"
+function actualizarCarrito() {
+    contadorCarrito.innerText = carritoDeCompras.reduce(
+        (acc, el) => acc + el.cantidad, 0
+    );
+    precioTotal.innerText = carritoDeCompras.reduce((acc, el) =>
+    acc + el.price * el.cantidad, 0);
+}
+
+/*function recuperar(){*/
+    let recuperarLS = JSON.parse(localStorage.getItem('carrito')) || []
+
+    /*
+    if (recuperarLS){
+        recuperarLS.forEach((el) =>{
+            mostrarCarrito(el);
+            carritoDeCompras.push(el);
+        actualizarCarrito();
+        });
+    }    
+}
+
+recuperar(); */
---
+++ archivos JS/Carrito/stock.js
@@ -0,0 +1,55 @@
+//  PRODUCTOS
+// TABLAS de SURF
+/*const Tablas = [
+    {id: 1, name: 'Blue Tiger', price: 120, tipo: 'surf', img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByTrtAbQU99Og8rJI5vn1niR1YApa2vYOq1WSmRuAKB_uO7AQwPSl3puTDgfUcGlcdSk&usqp=CAU'},
+    {id: 2, name: 'Retrofish', price: 180, tipo: 'surf', img: ''},
+    {id: 3, name: 'Green Tiger', price: 120, tipo: 'surf', img: ''},
+    {id: 4, name: 'Kai Lenny Lava', price: 170, tipo: 'surf', img: ''},
+    {id: 5, name: 'Waves and sand', price: 170, tipo: 'surf', img: ''},
+    {id: 6, name: 'Greenroom', price: 170, tipo: 'surf', img: ''},
+]  
+console.log(Tablas)
+
+//SURFSKATE 
+
+const Surfskates = [
+    {id: 7, name: 'Blue Tiger', price: 120, tipo: 'surfskate', img: 'CarverFirefly2022C7.jpg'},
+    {id: 8, name: 'Retrofish', price: 180, tipo: 'surfskate', img: ''},
+    {id: 9, name: 'Green Tiger', price: 120, tipo: 'surfskate', img: ''},
+    {id: 10, name: 'Kai Lenny Lava', price: 170, tipo: 'surfskate', img: ''},
+    {id: 11, name: 'Waves and sand', price: 170, tipo: 'surfskate', img: ''},
+    {id: 12, name: 'Greenroom', price: 170, tipo: 'surfskate', img: ''},
+]  
+console.log(Surfskates);
+
+//REMERAS
+
+const Remeras = [
+    {id:13, name: 'Blue Waves', price:35, tipo: 'remera', img:''},
+    {id:14, name: 'Green Waves', price:20, tipo: 'remera', img:''},
+    {id:15, name: 'White Waves', price:70, tipo: 'remera', img:''},
+    {id:16, name: 'Cornsilk Waves', price:58, tipo: 'remera', img:''},
+    {id:17, name: 'Red Waves', price:35, tipo: 'remera', img:''},
+]
+console.log(Remeras);*/
+
+const stockProductos = [
+    {id: 1, name: 'Blue Tiger', price: 120, tipo: 'surf', img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSByTrtAbQU99Og8rJI5vn1niR1YApa2vYOq1WSmRuAKB_uO7AQwPSl3puTDgfUcGlcdSk&usqp=CAU'},
+    {id: 2, name: 'Retrofish', price: 180, tipo: 'surf', img: 'https://www.surfmarket.org/images/stories/virtuemart/product/PLEZPHEZ-almond.jpg'},
+    {id: 3, name: 'Green Tiger', price: 120, tipo: 'surf', img: 'https://www.tradeinn.com/h/13878/138782009/almond-tabla-surf-plez-phez-r-series-64.jpg'},
+    {id: 4, name: 'Kai Lenny Lava', price: 170, tipo: 'surf', img: 'https://www.surfmarket.org/images/stories/virtuemart/product/SANOSPECIAL-almond.jpg'},
+    {id: 5, name: 'Waves and sand', price: 170, tipo: 'surf', img: 'https://www.tradeinn.com/h/13878/138782018/almond-tabla-surf-secret-menu-r-series-54.jpg'},
+    {id: 6, name: 'Greenroom', price: 170, tipo: 'surf', img: 'https://www.surfmarket.org/images/stories/virtuemart/product/LOGISTIC-6297-97.jpg'},
+    {id: 7, name: 'Blue Tiger', price: 120, tipo: 'surfskate', img: 'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/1280x1280/products/599/3404/CarverKaiDragonCX__63356.1640719251.jpg?c=1'},
+    {id: 8, name: 'Retrofish', price: 180, tipo: 'surfskate', img: 'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/600x600/products/606/3411/CarverKnoxPhoenixC7__25843.1640720594.jpg?c=1'},
+    {id: 9, name: 'Green Tiger', price: 120, tipo: 'surfskate', img: 'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/600x600/products/616/3423/CarverGreenroom2022C7__07923.1640723892.jpg?c=1'},
+    {id: 10, name: 'Kai Lenny Lava', price: 170, tipo: 'surfskate', img: 'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/380x380/products/614/3421/CarverSwallow2022C7__97455.1640723414.jpg?c=1'},
+    {id: 11, name: 'Waves and sand', price: 170, tipo: 'surfskate', img: 'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/380x380/products/612/3419/CarverFirefly2022C7__47784.1640722841.jpg?c=1'},
+    {id: 12, name: 'Greenroom', price: 170, tipo: 'surfskate', img:'https://cdn11.bigcommerce.com/s-icfw6t6sn3/images/stencil/380x380/products/610/3417/CarverResin2022C7__09420.1640722581.jpg?c=1'},
+    {id:13, name: 'Blue Waves', price:35, tipo: 'remeras', img:'https://underwavebrand.com/wp-content/uploads/2022/04/DSC03827-710x1078.jpg'},
+    {id:14, name: 'Green Waves', price:20, tipo: 'remeras', img: 'https://underwavebrand.com/wp-content/uploads/2022/03/IMG_0064-min-500x750.jpg'},
+    {id:15, name: 'White Waves', price:70, tipo: 'remeras', img:'https://underwavebrand.com/wp-content/uploads/2022/03/DSC02650-500x750.jpg'},
+    {id:16, name: 'Cornsilk Waves', price:58, tipo: 'remeras', img:'https://underwavebrand.com/wp-content/uploads/2022/03/DSC02516-500x750.jpg'},
+    {id:17, name: 'Red Waves', price:35, tipo: 'remeras', img:'https://underwavebrand.com/wp-content/uploads/2022/03/IMG_0098-500x750.jpg'},
+]
+console.log(stockProductos);
